dfc51a18cb0a536f6ab969bf887c0ae1138f898d
Rayen <rayen.bakali@gmail.com>
2022-12-14 15:08:12 +0100

    Fixed  pagination

--- src/app/departement/departementform/departementform.component.ts
+++ src/app/departement/departementform/departementform.component.ts
@@ -6,47 +6,56 @@ import { DepartementService } from 'src/app/core/service/departement.service';
 @Component({
   selector: 'app-departementform',
   templateUrl: './departementform.component.html',
-  styleUrls: ['./departementform.component.scss']
+  styleUrls: ['./departementform.component.scss'],
 })
 export class DepartementformComponent implements OnInit {
+  public departement: Departement;
+  public action: String;
 
-  public departement:Departement;
-  public action:String;
+  public msg = '';
 
-  constructor(private departementService:DepartementService,private route:Router,private currentroute:ActivatedRoute) { }
+  constructor(
+    private departementService: DepartementService,
+    private route: Router,
+    private currentroute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    let id =this.currentroute.snapshot.params['idDepart'];
-    if(id!=null){
-      this.departementService.getdepbyid(id).subscribe(
-       
-        (data:Departement)=>{this.departement=data}
-      );
+    let id = this.currentroute.snapshot.params['idDepart'];
+    if (id != null) {
+      this.departementService.getdepbyid(id).subscribe((data: Departement) => {
+        this.departement = data;
+      });
       //update
-      this.action='update';
-    }else{
+      this.action = 'update';
+    } else {
       //add
-      this.departement=new Departement()
-      this.action='ADD';
+      this.departement = new Departement();
+      this.action = 'ADD';
     }
-
   }
 
-  savedepartement(){
-    if(this.action=='update'){
+  savedepartement() {
+    if (this.action == 'update') {
       this.departementService.updatDep(this.departement).subscribe(
-        ()=>this.route.navigate(['/departement']),
-        ()=>{console.log('error'),
-        ()=>{console.log('complete')}}
-      )
-
-    }else{
-    this.departementService.adddep(this.departement).subscribe(
-      ()=>this.route.navigate(['/departement']),
-      ()=>{console.log('error'),
-      ()=>{console.log('complete')}}
-    )
+        () => this.route.navigate(['/departement']),
+        () => {
+          console.log('error'),
+            () => {
+              console.log('complete');
+            };
+        }
+      );
+    } else {
+      this.departementService.adddep(this.departement).subscribe(
+        () => this.route.navigate(['/departement']),
+        () => {
+          console.log('error'),
+            () => {
+              console.log('complete');
+            };
+        }
+      );
     }
   }
-
 }
